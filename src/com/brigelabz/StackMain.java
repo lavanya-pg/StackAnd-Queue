3e83b1979a190116a6916d0fef1f221ca1dd8da8
lavanya-pg <lavanyapg8@gmail.com>
2021-07-03 07:03:41 +0530

    ADDED:Creating a Stack

--- src/com/brigelabz/StackMain.java
+++ src/com/brigelabz/StackMain.java
@@ -5,7 +5,8 @@ public class StackMain
 	public static void main(String[] args)
 	{
 
-		System.out.println("Welcome to the Stack  Program");
+		System.out.println("Welcome to the Stack and Queue Program");
+		System.out.println("Welcome to Stack  Program");
 		Stack stack = new Stack();
 		
 		stack.push(70);
