f7f3d2b4773586e4bfb777198fadb8557f3fd110
lavanya-pg <lavanyapg8@gmail.com>
2021-07-03 06:54:53 +0530

    Initial Commit

---
+++ src/com/brigelabz/StackMain.java
@@ -0,0 +1,18 @@
+package com.brigelabz;
+
+public class StackMain
+{
+	public static void main(String[] args)
+	{
+
+		System.out.println("Welcome to the Stack  Program");
+		Stack stack = new Stack();
+		
+		stack.push(70);
+		stack.push(30);
+		stack.push(56);
+		
+		stack.print();
+
+	}
+}
